15cfba0528494fb95a805934e873bca4eb5872f6
ANDERSOUNDZ <andersonmikol@live.com>
2026-02-21 19:29:53 -0500

    Levantando Ecosistema 1.3: Transaction_Services LEvantado, lógica funcional agregada, validaciones agregadas en products validator

--- backend_infrastructure/product_service/product_service/adapters/input/validators/product/CreateProductValidator.cs
+++ backend_infrastructure/product_service/product_service/adapters/input/validators/product/CreateProductValidator.cs
@@ -7,10 +7,18 @@ namespace product_service.adapters.input.validators.product
     {
         public CreateProductValidator()
         {
-            RuleFor(x => x.Name).NotEmpty().MaximumLength(100);
-            RuleFor(x => x.Price).GreaterThan(0);
-            RuleFor(x => x.Stock).GreaterThanOrEqualTo(0);
-            RuleFor(x => x.Category).NotEmpty();
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("El nombre del producto es obligatorio.")
+                .MaximumLength(100).WithMessage("El nombre no puede exceder los 100 caracteres.");
+            RuleFor(x => x.Price)
+                .GreaterThan(0).WithMessage("El precio debe ser un valor mayor a cero.");
+            RuleFor(x => x.Stock)
+                .GreaterThanOrEqualTo(0).WithMessage("El stock no puede ser un número negativo.");
+            RuleFor(x => x.Category)
+                .NotEmpty().WithMessage("La categoría es obligatoria.")
+                .MaximumLength(50).WithMessage("La categoría no debe exceder los 50 caracteres.");
+            RuleFor(x => x.Image)
+                .MaximumLength(500).WithMessage("La URL de la imagen es demasiado larga.");
         }
     }
 }
